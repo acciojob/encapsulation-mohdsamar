ec7ed01072c0049f0b1e483977a5daace1e9cb2f
Mohd Samar <samaramu32@gmail.com>
2023-03-17 09:41:11 +0530

    assignment done

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String Name;
+    public String getName(){
+        return Name;
+    }
+    public String setName(String Name){
+        return this.Name=Name;
+    }
 }
